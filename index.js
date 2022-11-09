9dbc1c3375512f6560e724fb69892a9adb998da9
nathanielmuci <nate.muci@gmail.com>
2022-11-08 23:54:20 -0600

    added a few questions and revised old items

--- index.js
+++ index.js
@@ -5,43 +5,74 @@ inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is the title of your webpage?',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'description',
+      message: 'Please write a breif description about your webpage.',
       
     },
     {
       type: 'list',
-      message: 'What is your preferred season',
-      name: 'seasons',
-      choices: ['Fall', 'Summer', 'Winter', 'Spring'],
+      name: 'license',
+      message: 'Which type of license are you using?',
+      choices: ['MIT', 'Drivers', 'Boating', 'Hunting'],
     },
   ])
   .then((data) => {
     console.log(data);
 
-    fs.writeFile("index.html", layOut(data), (err) =>
+    fs.writeFile("README.md", layOut(data), (err) =>
       (err) ? console.log(err) : console.log('Success!'))
     
   });
 
-  const layOut = ({location, seasons, name}) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Hello my name is ${name}!</h2>
-    <h2>I was born in ${location}</h2>
-    <h2>My favorite season is ${seasons}</h2>
+  const layOut = ({license, title, description}) =>
+  `# ${title}
 
-</body>
-</html>`;
+  ## Description
+  
+  ${description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributers](#contributers)
+  - [License](#license)
+  - [Test](#test)
+  - [Contact](#contact)
+  
+  ## Installation
+  
+  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+  
+  ## Usage
+  
+  Provide instructions and examples for use. Include screenshots as needed.
+  
+  To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+  
+      md
+      ![alt text](assets/images/screenshot.png)
+      
+  
+  ## Contributers
+  
+  List your collaborators, if any, with links to their GitHub profiles.
+  
+  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+  
+  If you followed tutorials, include links to those here as well.
+  
+  ## License
+  
+  ${license}
+  
+  
+  ## Tests 
+  
+  ## Contact`;
+  //the original README.md template was modeled after the codeing bootcamp template from: (https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
